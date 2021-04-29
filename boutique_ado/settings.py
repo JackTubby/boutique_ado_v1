28d114df6be8bf3cdfd0d8922131975f8461d568
JackTubby <jack200034@hotmail.com>
2021-04-29 17:07:08 +0000

    Deployment - AWS bucket - Fix Typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,11 +177,11 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'mxhaib-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_KEY = os.environ.get('AWS_ACCESS_KEY_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custon_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
